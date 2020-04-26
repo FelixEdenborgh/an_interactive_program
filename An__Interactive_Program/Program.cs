5f7ea94eb5e7b7a1be91bae902e935669c1b9e8e
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:11:49 +0200

    Add files via upload

---
+++ An__Interactive_Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace An__Interactive_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            while (true)
+            {
+                Console.WriteLine("Enter an value: ");
+                value = Convert.ToInt32(Console.ReadLine());
+
+                if (value < 10)
+                {
+                    Console.WriteLine("Your number is to small: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Your number is greater then 10: " +value);
+                }
+
+            }
+
+        }
+    }
+}
